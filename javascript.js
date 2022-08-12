91ca7ec1ec49558511e90f5995f9ad0441bd030e
Draxgun <j.alejandro.banuelos@outlook.com>
2022-08-12 11:58:31 -0600

    change sin the menu that can make work the info

--- javascript.js
+++ javascript.js
@@ -9,9 +9,17 @@ const gameBoard = (() =>{
 
     let renderGrid = (board) => {
         
+        /* In charge of deleting the menu */
+        let content = document.querySelector('.content');
+        content.innerHTML = '';
+        console.log('hola')
+
+        /* Creates the container for the board */
         let boardContainer = document.createElement('div')
         boardContainer.classList.add("board")
-        console.log('hola')
+        content.appendChild(boardContainer)
+        
+        
         for(let i=0; i<=8;i++) {
             /* Creates a 3 by 3 grid */        
             let grid = document.createElement('div');
@@ -21,15 +29,18 @@ const gameBoard = (() =>{
             boardContainer.appendChild(grid);  
             boardContainer.style.gridTemplateColumns = `repeat(${3},1fr)`
         }
+
         let grids = document.querySelectorAll('.grid')
         grids.forEach(grid => {
+            console.log(grid.textContent)
             grid.addEventListener('click',() =>{
-                grid.textContent = document.querySelector('.playerMarker').textContent
+                grid.textContent = 'x'
                 grid.classList.remove('grid')
                 grid.classList.add('ocuppiedGrid')
                 console.log(checkForWinner())
             })
         });
+        
 
     }
 
@@ -100,6 +111,7 @@ const gameBoard = (() =>{
         if(results.includes(true)){
             winner = 'x';
             gameOver = true;
+            alert('x won!')
         return winner
         } else{
             winningCombinations.forEach(winningCombination => {
@@ -140,32 +152,58 @@ const player = (name,marker) => {
     return {name,marker}
 }
 
-let marker  = document.querySelector('.playerMarker')
-
-marker.addEventListener('click', () => {
-    if (marker.textContent === 'x'){
-        marker.textContent = 'o'
-    } else {
-        marker.textContent = 'x'
-        console.log('hola')
-    }
-})
-
 const menu = (() =>{
 
 
     let pvpMode = document.getElementById('pvpButton')
     pvpMode.addEventListener('click',() =>{
-        let gameMode = 'pvp'
+        let menuContents = document.querySelector('.menuContents')
+        menuContents.innerHTML = '';
+
+        let player1Marker = document.createElement('div')
+        player1Marker.classList.add('playerTitle')
+        player1Marker.textContent = 'Player 1: X'
+        menuContents.appendChild(player1Marker)
+
+        let player1Name = document.createElement('div')
+        player1Name.classList.add('playerTitle')
+        player1Name.textContent = 'Player name'
+        menuContents.appendChild(player1Name)
+
+        let player1Input = document.createElement('input')
+        player1Input.setAttribute('type','input')
+        player1Input.classList.add('menuInput')
+        menuContents.appendChild(player1Input)
+
+        let player2Marker = document.createElement('div')
+        player2Marker.classList.add('playerTitle')
+        player2Marker.textContent = 'Player 2: 0'
+        menuContents.appendChild(player2Marker)
+
+        let player2Name = document.createElement('div')
+        player2Name.classList.add('playerTitle')
+        player2Name.textContent = 'Player name'
+        menuContents.appendChild(player2Name)
+
+        let player2Input = document.createElement('input')
+        player2Input.setAttribute('type','input')
+        player2Input.classList.add('menuInput')
+        menuContents.appendChild(player2Input)
+
+        let start = document.createElement('button');
+        start.classList.add('menuButton')
+        start.setAttribute('id','startButton')
+        start.textContent = 'Play!'
+        menuContents.appendChild(start)
+        start.addEventListener('click',() =>{
+            gameBoard.renderGrid(gameBoard.board)
+        })
+
+
     })
 
-    let getGameMode = gameMode
-
-    return{
-        getGameMode,
 
 
-    }
 
 })();
 
